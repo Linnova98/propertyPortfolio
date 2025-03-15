3d35dd57d6d392be03dc958948cea02c349a19c6
Linnova <92028792+Linnova98@users.noreply.github.com>
2025-03-15 19:20:34 +0100

    Changed debug to false

--- propertyPortfolio/settings.py
+++ propertyPortfolio/settings.py
@@ -9,9 +9,9 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 SECRET_KEY = env("SECRET_KEY")
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
